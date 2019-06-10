2b5e396c2c3156c4d65a6d4d80c092e35875bfc9
Defelo <elodef42@gmail.com>
2019-06-10 20:35:16 +0200

    Use yaw, pitch and roll instead of screen coordinates

--- py3d.py
+++ py3d.py
@@ -1,26 +1,50 @@
+import math
 from typing import Tuple, List
 
 from PIL import Image, ImageDraw
 
+import vector
 from lineqsysolve import solve
+
+FOV: float = 60
 
 Point3D = Tuple[float, float, float]
 Point = Tuple[float, float]
 
 
-def project(c: Point3D, p: Point3D, a: Point3D, b: Point3D, d: Point3D) -> Point:
-    # (p-c)*s + (b-a)*t + (d-b)*u = c-a
+def raycast(source: Point3D, yaw: float, pitch: float, d: float) -> Point3D:
+    yaw: float = yaw * math.pi / 180
+    pitch: float = pitch * math.pi / 180
+
+    x: float = math.cos(yaw) * d
+    y: float = math.sin(pitch) * d
+    z: float = math.sin(yaw) * d
+
+    return source[0] + x, source[1] + y, source[2] + z
+
+
+def project(c: Point3D, p: Point3D, yaw: float, pitch: float, roll: float) -> Point:
+    a: Point3D = raycast(c, yaw + FOV / 2, pitch + FOV / 2, 100)
+    b: Point3D = raycast(c, yaw - FOV / 2, pitch + FOV / 2, 100)
+    d: Point3D = raycast(c, yaw - FOV / 2, pitch - FOV / 2, 100)
+
+    # c + (p-c)*s = a + (b-a)*t + (d-b)*u
+    # (c-p)*s + (b-a)*t + (d-b)*u = c-a
     matrix: List[List[float]] = []
     for i in range(3):
         row: List[float] = [
-            p[i] - c[i],
+            c[i] - p[i],
             b[i] - a[i],
             d[i] - b[i],
             c[i] - a[i]
         ]
         matrix.append(row)
     _, t, u = solve(matrix)
-    return t, u
+
+    vec: vector.Vector = vector.Point(t - 0.5, u - 0.5).to_vector()
+    vec.angle -= roll
+    t, u = vec.to_point()
+    return t + 0.5, u + 0.5
 
 
 def cube():
@@ -31,18 +55,16 @@ def cube():
 
     black = (0, 0, 0)
 
-    camera = (0, 0, 0)
-    plane = [
-        (-wid / 2, hei / 2, 100),
-        (wid / 2, hei / 2, 100),
-        (wid / 2, -hei / 2, 100)
-    ]
+    draw.line((wid / 2, 0, wid / 2, hei), fill=black, width=1)
+    draw.line((0, hei / 2, wid, hei / 2), fill=black, width=1)
+
+    camera: Point3D = (0, 0, 0)
 
     for i in range(2):
         for j in range(2):
             for k in range(2):
-                point = (20 * i - 10, 20 * j - 10, 20 * k + 40)
-                x, y = project(camera, point, *plane)
+                point: Point3D = (20 * i - 10, 20 * j - 10, 20 * k + 40)
+                x, y = project(camera, point, 90, 0, 0)
                 draw.point((x * wid, y * hei), fill=black)
 
     image.save("output.png")
